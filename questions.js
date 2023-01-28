d73d17d2e8fce3dfdede3672ffbc815ea9e32459
Joaquin Loya <118794860+JLoya900@users.noreply.github.com>
2023-01-27 16:04:44 -0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,48 @@
-
+var questions = [
+  {
+    question: "What is the '$' called in jQuery?",
+    choices: ["Bling Code", "Dollar Sign", "Big Operator", "Bling Operator"],
+    correctAnswer: 3
+  },
+  {
+    question: "What does DOM stand for?",
+    choices: [
+      "Document Objective Manager",
+      "Document Object Model",
+      "Disk Operating Model",
+      "Dominant Object Mistress"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "Where can you place Javascript code?",
+    choices: [
+      "In the .js file and in the .html file",
+      "Only in the .js file.",
+      "Anywhere you want.",
+      "Only in the header."
+    ],
+    correctAnswer: 0
+  },
+  {
+    question:
+      "These 3 languages are considered the foundational languages of the web development.",
+    choices: [
+      "English, Spanish and French",
+      "HTML, CSS and JavaScript",
+      "See, Plus Plus and ASP",
+      "Dunning, Latin and Togo"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "What can JavaScript be used for?",
+    choices: [
+      "Access APIs.",
+      "Look into the future.",
+      "Manipulate the trays.",
+      "Clear the room."
+    ],
+    correctAnswer: 0
+  }
+];
